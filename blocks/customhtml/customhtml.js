0e7818b4d341e12fffdd85fdc43f80586bb6d3b3
Elizabeth Franklin <elizabethwfranklin@gmail.com>
2024-10-11 14:08:25 -0400

    Modify tab spacing

--- blocks/customhtml/customhtml.js
+++ blocks/customhtml/customhtml.js
@@ -1,21 +1,20 @@
 export default function decorate(block) {
-    const cols = [...block.firstElementChild.children];
-    block.classList.add(`columns-${cols.length}-cols`);
-  
-    // setup image columns
-    [...block.children].forEach((row) => {
-      [...row.children].forEach((col) => {
-        col.innerHTML = `
-            <div class="banner-text">
-                <p class="large-text">Caramels gummi bears bonbon danish gummi bears gingerbread.</p>
-                <p>Jelly cupcake I love I love cookie cookie bear claw.</p>
-                <a class="primary-btn" href="https://www.youtube.com/@AEMGeeks" target="_blank">Muffin tiramisu</a>
-                <p class="primary-text">tootsie roll gummies sweet roll.</p>
-            </div>
-            <div class="banner-image">
-            </div>
-        `
-      });
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      col.innerHTML = `
+        <div class="banner-text">
+          <p class="large-text">Caramels gummi bears bonbon danish gummi bears gingerbread.</p>
+            <p>Jelly cupcake I love I love cookie cookie bear claw.</p>
+            <a class="primary-btn" href="https://www.youtube.com/@AEMGeeks" target="_blank">Muffin tiramisu</a>
+            <p class="primary-text">tootsie roll gummies sweet roll.</p>
+        </div>
+        <div class="banner-image">
+        </div>
+      `;
     });
-  }
-  
+  });
+}
